e23dcf1c8c9feecf489de45badaa6659d5a491c4
Anastasia <aburlakova45@gmail.com>
2023-07-14 15:53:06 +0300

    вызов всех функций в main

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils import read_file, get_filtered_data, get_sorted_data, get_formatted
+
+
+def main():
+    file = 'operations.json'
+    data = read_file(file)
+    data = get_filtered_data(data)
+    data = get_sorted_data(data)
+    data = get_formatted(data)
+    for item in data:
+        print(item)
+
+
+
+if __name__ == '__main__':
+    main()
