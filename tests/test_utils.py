5eee1b7b1d00b6805d5ad492d4ed0dcd955c4b36
Anastasia <aburlakova45@gmail.com>
2023-07-14 16:49:03 +0300

    тест для функции get_sorted_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,25 @@
-from utils import read_file, get_filtered_data
+from utils import read_file, get_filtered_data, get_sorted_data
 import pytest
 
 
 @pytest.fixture
 def get_test_data():
     data = (
+        {
+            "id": 214024827,
+            "state": "CANCELED",
+            "date": "2018-12-20T16:43:26.929246",
+            "operationAmount": {
+                "amount": "70946.18",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 10848359769870775355",
+            "to": "Счет 21969751544412966366"
+        },
         {
             "id": 441945886,
             "state": "EXECUTED",
@@ -34,7 +49,8 @@ def get_test_data():
             "description": "Перевод организации",
             "from": "MasterCard 7158300734726758",
             "to": "Счет 35383033474447895560"
-        }
+        },
+        {}
     )
     return data
 
@@ -45,3 +61,8 @@ def test_read_file():
 
 def test_get_filtered_data(get_test_data):
     assert len(get_filtered_data(get_test_data)) == 2
+
+
+def test_get_sorted_data(get_test_data):
+    data = get_filtered_data(get_test_data)
+    assert get_sorted_data(data) == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}]
