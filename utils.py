5e09ad6ba1a87e0771f869ce296e4f6d1d5bef99
Anastasia <aburlakova45@gmail.com>
2023-07-14 14:39:06 +0300

    функция get_filtered_data

--- utils.py
+++ utils.py
@@ -6,3 +6,11 @@ def read_file(file):
         new_file = json.load(f)
     return new_file
 
+
+def get_filtered_data(data):
+    new_data = []
+    for item in data:
+        if 'state' in item.keys():
+            if item["state"] == 'EXECUTED':
+                new_data.append(item)
+    return new_data
