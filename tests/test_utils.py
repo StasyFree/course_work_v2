0823d839e381879731d87913de0037af0ebac7fa
Anastasia <aburlakova45@gmail.com>
2023-07-14 17:00:14 +0300

    тест функции get_formatted

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from utils import read_file, get_filtered_data, get_sorted_data
+from utils import read_file, get_filtered_data, get_sorted_data, put_on_mask, get_formatted
 import pytest
 
 
@@ -66,3 +66,13 @@ def test_get_filtered_data(get_test_data):
 def test_get_sorted_data(get_test_data):
     data = get_filtered_data(get_test_data)
     assert get_sorted_data(data) == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}]
+
+
+def test_put_on_mask():
+    assert put_on_mask('Maestro 1596837868705199') == "Maestro 1596 83** **** 5199"
+
+
+def test_get_formatted(get_test_data):
+    data = get_filtered_data(get_test_data)
+    data = get_sorted_data(data)
+    assert get_formatted(data) == ['26.08.2019 Перевод организации \nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб.\n', '03.07.2019 Перевод организации \nMasterCard 7158 30** **** 6758 -> Счет **5560\n8221.37 USD\n']
