1f2974a0c8c948f8538bf011fa27fc71a7fd480c
Anastasia <aburlakova45@gmail.com>
2023-07-14 16:35:45 +0300

    тест для функции get_filtered_data и добавление фикстуры

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,47 @@
 from utils import read_file, get_filtered_data
+import pytest
+
+
+@pytest.fixture
+def get_test_data():
+    data = (
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+                "amount": "8221.37",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+        }
+    )
+    return data
 
 
 def test_read_file():
     assert isinstance(read_file('operations.json'), list)
 
+
+def test_get_filtered_data(get_test_data):
+    assert len(get_filtered_data(get_test_data)) == 2
